9f40d164ef5291b8c121450f52e99a4e16b18a22
Victorv88 <33388239+Victorv88@users.noreply.github.com>
2018-07-13 18:55:45 +0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+	$con=mysqli_connect("localhost", "id6481225_friends_database", "Nuegrabaman8","id6481225_friends_database");
+	$username=$_POST["username"];
+	$password=$_POST["password"];
+	$mail=$_POST["mail"];
+	$question1=$_POST["question1"];
+	$ans11=$_POST["ans11"];
+	$ans12=$_POST["ans12"];
+	$ans13=$_POST["ans13"];
+	$ans14=$_POST["ans14"];
+	
+	$statement=mysqli_prepare($con,"INSERT INTO user(username,mail,password,question1
+	,ans11,ans12,ans13,ans14) VALUES (?,?,?,?,?,?,?,?)");
+	
+	mysqli_stmt_bind_param($statement,"siss", $username, $mail, $password, $question1,
+	$ans11, $ans12, $ans13, $ans14);
+	mysqli_stmt_execute($statement);
+	
+?>
